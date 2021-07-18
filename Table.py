3fc02cda999e6356e3d59ece6729596cb1763301
irena <irena.vent@gmail.com>
2021-07-18 19:01:17 +0200

    test push

--- Table.py
+++ Table.py
@@ -4,5 +4,6 @@ table = PrettyTable() #we have a table object
 
 table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
 table.add_column("Type", ["Electric", "Water", "Fire"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
 
 print(table)
